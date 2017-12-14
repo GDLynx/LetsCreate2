a66cb3e963fa41b8cb1a7be879b0c84fb8fabfd7
GDLynx <lynxthatthinks@gmail.com>
2017-12-14 21:57:18 +0000

    Add files via upload

--- objects/present.js
+++ objects/present.js
@@ -10,7 +10,7 @@ function Present() {
     };
     this.position = {
         // x: 200, y: 200
-        x: 20 + Math.random() * 400, y: 300 + Math.random() * 100  
+        x: 0 + Math.random() * 300, y: 300 + Math.random() * 100  
     };
     this.width = 30;
     this.height = 30;
