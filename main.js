087fea6e3e3452976fdfa7132e3644645e560141
GDLynx <lynxthatthinks@gmail.com>
2017-12-15 17:28:11 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /// General Set-up
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-canvas.style.cssText = "background-color:white;border:2px solid black;";
+canvas.style.cssText = "background-color:cyan;border:2px solid black;";
 
 /// Global Variables
 var canvasWidth = canvas.width, canvasHeight = canvas.height;
@@ -11,6 +11,7 @@ var canvasWidth = canvas.width, canvasHeight = canvas.height;
 var controller;
 var house = [];
 var present = [];
+var snow = [];
 
 /// Ensure the correct request is made for Animation Frame
 if (!window.requestAnimationFrame) {
@@ -48,9 +49,16 @@ function init() {
     update();
 }
 
+var reindeerImageInstance;
+reindeerImageInstance = new Image();
 var increments = 0;
+var increments2 = 0;
 function update() {
+    if (snow.length < 105) {
+        snow.push(new Snow());
+    }
     increments += 1;
+    increments2 += 1;
     if (increments >= 100 && house.length < 1) {
         increments = 0;
         if (Math.floor(Math.random() * 10) > 5) {
@@ -71,14 +79,27 @@ function update() {
 
     /// Clear canvas + draw (to an empty canvas)
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    controller.display();
     for (var p = 0; p < present.length; p++) {
         present[p].display();
     }
+    if (increments2 % 120 === 0 && reindeerImageInstance == "./imagesreindeersleigh1.png") {
+        reindeerAndSleigh("./images/reindeersleigh2.png");
+        increments2 = 0;
+    } else {
+        reindeerAndSleigh("./images/reindeersleigh1.png");
+    }
+    for (var s = 0; s < snow.length; s++) {
+        snow[s].display();
+    }
+    if (snow.y > canvasHeight) {
+        snow.slice(0, 1);
+    }
+    controller.display();
     //instancePerformBehavior(house, house.display);
     for (var h = 0; h < house.length; h++) {
         house[h].display();
     }
+    displayGround();
     // instancePerformBehavior(house, house.display);
     // instanceBehave(house, "display");
 
@@ -88,9 +109,9 @@ function update() {
             if (house[h].intersects(present[p])) {
                 // alert("X: " + present[p].position.x + "Y: " + present[p].position.y);
                 if (present[p].presentType == "explosive") {
-                    controller.score += 1;
-                } else {
                     controller.score -= 1;
+                } else {
+                    controller.score += 1;
                 }
                 newPresent();
             }
@@ -101,17 +122,18 @@ function update() {
 
     /// Destroy explosive present after X (6) seconds - should be 6 though is currently 3
     for (var p = 0; p < present.length; p++) {
-        if (present[p].presentType == "explosive") {
-            if (increments % 3 === 2) {
-                console.log(increments % 3); // for testing
-                newPresent();
-            }
+        if (present[p].presentType == "explosive" && increments % 120 === 0) {
+            //alert(increments);
+            //console.log(increments % 6); // for testing
+            newPresent();
         }
     }
 
     /// Game Loop
+    //console.log(reindeerImage);
     requestAnimationFrame(update);
 }
+reindeerAndSleigh("./images/reindeersleigh1.png");
 
 /// Run Initiallization function which will begin the game
 init();
@@ -119,19 +141,18 @@ init();
 // var t = 0;
 function newPresent() {
 //    console.log(_t);
-    var presentType = Math.floor(Math.random() * 4); // 1 = present 2 = explodingPresent
-    console.log(presentType);
+    var randType = Math.floor(Math.random() * 4); // 1 = present 2 = explodingPresent
+    //console.log(presentType);
     for (var p = 0; p < present.length; p++) {
         present.splice(0, 1);
-
-            console.log("New present");
+            //console.log("New present");
             present.push(new Present());
-            if (presentType < 2) {
+            if (randType < 2) {
                 present[p].presentType = "regular";
             } else {
                 present[p].presentType = "explosive";
             }
-            console.log(present[p].presentType);
+            // console.log(present[p].presentType);
     }
 }
 
@@ -159,3 +180,31 @@ function instancePerformBehavior(object, instanceAndBehavior) {
     }
 }
 */
+
+function reindeerAndSleigh(imageSrc) {
+    // var image = new Image();
+    reindeerImageInstance.src = imageSrc;
+    ctx.drawImage(reindeerImageInstance, canvasWidth/2-90, 300);
+    ctx.globalCompositeOperation="destination-over";
+}
+function Snow() {
+    this.x = Math.random() * 1300;
+    this.y = 0;
+
+    this.display = function() {
+        this.y += 1;
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.arc(this.x, this.y, 5,0, 2*Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+function displayGround() {
+    console.log("Ground");
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 60, canvasWidth, canvasHeight);
+    ctx.fill();
+    ctx.closePath();
+}
